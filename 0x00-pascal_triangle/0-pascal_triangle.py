91e8a1325d057c3757e492c6ee8169ad6cd8c9d3
TatendaTorerwa <ttorerwa01@gmail.com>
2024-04-25 18:36:52 +0000

    0. Pascal's Triangle.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,11 +8,9 @@ def pascal_triangle(n):
     triangle = []
     for i in range(n):
         row = []
-        for space in range(1, n - i):
-            row.append(0)  # Add placeholder 0s for formatting
         for r in range(i + 1):
-            ncr = factorial(i) // (factorial(r) * factorial(i - r))
-            row.append(ncr)
+            element = factorial(i) // (factorial(r) * factorial(i - r))
+            row.append(element)
         triangle.append(row)
 
     return triangle
