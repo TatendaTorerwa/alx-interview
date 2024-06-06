795c20776abc4fa3a55b130a6ba839a66cfeba7b
TatendaTorerwa <ttorerwa01@gmail.com>
2024-06-06 08:23:56 -0700

    0. Star Wars Characters.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,52 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-// Get the Movie ID from the command line arguments
-const movieId = process.argv[2];
-if (!movieId) {
-  console.log('Usage: ./0-starwars_characters.js <movieId>');
-  process.exit(1);
-}
-
-// URL of the Star Wars API endpoint for films
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-request(url, (error, response, body) => {
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, async function (error, response, body) {
   if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode === 200) {
-    // Parse the response body to JSON
-    const data = JSON.parse(body);
-    const characters = data.characters;
-
-    // Create an array of promises for fetching character details
-    const characterPromises = characters.map(characterUrl => {
-      return new Promise((resolve, reject) => {
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else if (response.statusCode === 200) {
-            resolve(JSON.parse(body).name);
+    return console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[character], (err, res, html) => {
+          if (err) {
+            reject(err);
           } else {
-            reject(`Failed to fetch character: ${response.statusCode}`);
+            resolve(JSON.parse(html).name);
           }
         });
       });
-    });
-
-    // Use Promise.all to wait for all promises to resolve
-    Promise.all(characterPromises)
-      .then(names => {
-        names.forEach(name => console.log(name));
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  } else {
-    console.error(`Failed to fetch movie: ${response.statusCode}`);
+      console.log(res);
+    }
   }
 });
